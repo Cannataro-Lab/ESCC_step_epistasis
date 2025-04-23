01f04980dd8be5c3fcbe5931d473e9b0b0ee876b
glasmacherk <glasmacherk@emmanuel.edu>
2025-04-23 09:09:27 -0400

    add confidence interval calculations (due to package version changes), added significance to plot and removed vertical grey lines in panels

--- output_data/Figure_2_selection_coefficient_plot.R
+++ output_data/Figure_2_selection_coefficient_plot.R
@@ -28,65 +28,78 @@ selection_results <- selection_results %>%
 selection_results <- selection_results %>%
   mutate(gene = gsub("\\.1.*","",variant_name)) #extract gene name from variant_name
 
-pre <- selection_results[, .(variant = variant_name, ci_low_95_si_Pre, ci_high_95_si_Pre, variant_type, gene, si_Pre, progression = "Pre")] #filter normal samples
-pri <- selection_results[, .(variant = variant_name, ci_low_95_si_Pri, ci_high_95_si_Pri, variant_type, gene, si_Pri, progression = "Pri")] #filter tumor samples
 
-# Rename columns and combine data frames
-setnames(pre, c("si_Pre", "ci_low_95_si_Pre", "ci_high_95_si_Pre"),
-         c("selection_intensity", "ci_low_95", "ci_high_95"))
-setnames(pri, c("si_Pri", "ci_low_95_si_Pri", "ci_high_95_si_Pri"),
-         c("selection_intensity", "ci_low_95", "ci_high_95"))
+# Compute 95% CIs ----
+ci_results <- list()
 
-twostage_results <- rbind(pre, pri) 
+for (gene in genes) {
+  fit_list <- attr(cesa@selection_results[[gene]], "fit")
+  
+  fit <- fit_list[[1]]
+  
+  coef_vals <- coef(fit)
+  ci_vals <- tryCatch(
+    confint(fit, method = "quad"),  # Wald CI
+    error = function(e) matrix(rep(NA, 4), ncol = 2, byrow = TRUE,
+                               dimnames = list(c("si_Pre", "si_Pri"), c("2.5 %", "97.5 %")))
+  )
+  
+  ci_results[[gene]] <- data.frame(
+    gene = gene,
+    si_Pre = coef_vals["si_Pre"],
+    si_Pri = coef_vals["si_Pri"],
+    si_Pre_low = ci_vals["si_Pre", 1],
+    si_Pre_high = ci_vals["si_Pre", 2],
+    si_Pri_low = ci_vals["si_Pri", 1],
+    si_Pri_high = ci_vals["si_Pri", 2]
+  )
+}
 
+ci_df <- do.call(rbind, ci_results)
 
-# Restrict variants recurrent within each progression
-twostage_results_normal <- twostage_results[progression == "Pre"]
-twostage_results_primary <- twostage_results[progression == "Pri"]
+ci_df <- ci_df %>%
+  mutate(si_Pre_low = ifelse(si_Pre_low < 0.001, 0.001, si_Pre_low)) %>% # set 0.001 as lower bound for selection
+  mutate(si_Pri_low = ifelse(si_Pri_low < 0.001, 0.001, si_Pri_low))
+ci_df$gene <- factor(ci_df$gene, levels = unique(ci_df$gene))
 
+# Plot step-specific selection results ----
+pre_ci_df <- ci_df %>%
+  select(gene, selection_intensity = si_Pre, ci_low = si_Pre_low, ci_high = si_Pre_high) %>%
+  mutate(group = "Pre")
+pri_ci_df <- ci_df %>%
+  select(gene, selection_intensity = si_Pri, ci_low = si_Pri_low, ci_high = si_Pri_high) %>%
+  mutate(group = "Pri")
 
-stage_data <- data.frame(variant = twostage_results$variant,
-                         gene = twostage_results$gene,
-                         selection_intensity = twostage_results$selection_intensity,
-                         group = twostage_results$progression,
-                         ci_low = twostage_results$ci_low_95,
-                         ci_high = twostage_results$ci_high_95)
+stage_data <- rbind(pre_ci_df, pri_ci_df)
 
 stage_data <- stage_data %>%
-  mutate(ci_low_95 = if_else(is.na(ci_low), 0, ci_low)) #set lower bound for ci_low at 0 because it can't be negative
+  left_join(loglik_df, by = "gene") # add significance results from LRT
+stage_data$gene <- reorder(stage_data$gene, stage_data$p_value) # order by significance
 
+# define significance levels
 stage_data <- stage_data %>%
-  filter(gene %in% c("TP53", 
-                     "NOTCH1", 
-                     "NOTCH2", 
-                     "ERBB4", 
-                     "NFE2L2", 
-                     "PIK3CA", 
-                     "CDKN2A.p16INK4a",
-                     "CDKN2A.p14arf",
-                     "CDKN2A",
-                     "ARID1A", 
-                     "FAT1", 
-                     "EGFR",
-                     "ERBB2",
-                     "FBXW7",
-                     "FGFR3",
-                     "RB1",
-                     "SMAD4",
-                     "SOX2")) #select genes of interest (genes with clear trends in this case)
+  mutate(significance = case_when(
+    p_value <= 0.001 ~ "***",
+    p_value <= 0.01  ~ "**",
+    p_value <= 0.05  ~ "*",
+    p_value > 0.05   ~ "ns"
+  ))
 
-stage_data$gene <- factor(stage_data$gene, levels = unique(stage_data$gene))
-stage_data$gene <- reorder(stage_data$gene, -stage_data$selection_intensity)
+label_data <- stage_data %>%
+  group_by(gene) %>%
+  summarize(x = 1.5,
+            y = max(ci_high) * 1.05,
+            label = significance[1],  # same across Pre and Pri
+            .groups = "drop")
 
 selection_plots <- ggplot(stage_data, aes(x = group, y = selection_intensity, color = group)) +
   geom_point(size=2.5) +
-  geom_errorbar(aes(ymin = ci_low_95, ymax = ci_high, width = 0.25)) +
+  geom_errorbar(aes(ymin = ci_low, ymax = ci_high, width = 0.25)) +
   labs(x="Evolutionary trajectory", y="Scaled selection coefficient", color = "Tissue type") +
   scale_color_manual(labels = c("Normal", "Tumor"), values = c("#F8766D", "#00BFC4")) +
   theme_bw() +
   facet_wrap(~gene, ncol=4, scales = "free_y") +
   expand_limits(y = 0) +
-  geom_vline(xintercept = 1.5, lwd = 0.5, color = "lightgrey") +
   geom_hline(yintercept = 0, lwd = 0.5, color = "lightgrey", linetype = "dotted") +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
@@ -96,8 +109,15 @@ selection_plots <- ggplot(stage_data, aes(x = group, y = selection_intensity, co
         axis.title=element_text(size = 22),
         legend.position = "none",
         text = element_text(size = 22)) +
-  scale_x_discrete(labels= expression(O %->% N, N %->% T))
-selection_plots
+  scale_x_discrete(labels= expression(O %->% N, N %->% T)) +
+  geom_text(data = label_data,
+            aes(x = x, y = y, label = label),
+            inherit.aes = FALSE,
+            size = 6) + 
+  geom_text(data = label_data,
+            aes(x = x, y = y, label = label),
+            inherit.aes = FALSE,
+            size = 6)
 
 ggsave("output_data/fig_2_selection_plots.png", selection_plots, width=12.7, height=6.38)
 ggsave("output_data/fig_2_selection_plots.pdf", selection_plots, width=12.7, height=6.38)
